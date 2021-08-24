e258984c7d647cfac7021501924de2a17fe09126
Ben Johnston <docEbrown_github@protonmail.com>
2021-08-25 01:30:31 +1000

    feat: Print card id on load

--- src/js/connector.js
+++ src/js/connector.js
@@ -2,6 +2,12 @@ console.log("Hey");
 
 window.TrelloPowerUp.initialize({
   'card-badges': function (t, ops) {
-    return [];
+    return t.card("all")
+      .then(function (card) {
+        console.log(card);
+        return [{
+          text: card.idShort
+        }];
+      })
   }
 })
