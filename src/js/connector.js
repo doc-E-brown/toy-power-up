444df375c2b323894ab8c5f3ddb043ed37bd229b
Ben Johnston <docEbrown_github@protonmail.com>
2021-08-26 13:51:02 +1000

    feat: Add alert

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,5 +1,10 @@
 console.log("Hey");
 
+var t = window.TrelloPowerUp.iframe();
+t.alert({
+  'message': 'Hey hey'
+})
+
 window.TrelloPowerUp.initialize({
   'card-badges': function (t, ops) {
     return t.card("all")
