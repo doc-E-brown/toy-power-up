00c19273c1a3e3ee57bd551f57dd3a2ca9323f6d
Ben Johnston <docEbrown_github@protonmail.com>
2021-08-26 22:34:42 +1000

    feat: Log

--- src/js/search.js
+++ src/js/search.js
@@ -11,7 +11,7 @@ var btnCallback = function (t, opts) {
 
 window.TrelloPowerUp.initialize({
   'card-buttons': function (t, opts) {
-    console.log(JSON.stringify(t));
+    console.log("Msg " + JSON.stringify(t));
     return [{
       icon: GRAY_ICON,
       text: 'Seach',
