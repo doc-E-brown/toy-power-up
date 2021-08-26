b5d91596060dd73d8b45b20366cc00075ff4329d
Ben Johnston <docEbrown_github@protonmail.com>
2021-08-26 21:06:11 +1000

    feat: Add missing

--- src/js/card_back.js
+++ src/js/card_back.js
@@ -1,5 +1,11 @@
 var GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
 
+console.log("Loaded Card Back");
+
+var onBtnClick = function (t, opts) {
+  console.log('Someone clicked the button');
+};
+
 window.TrelloPowerUp.initialize({
   'card-back-section': function (t, options) {
     return {
@@ -15,5 +21,56 @@ window.TrelloPowerUp.initialize({
         },
       }
     };
+  },
+  'card-buttons': function (t, opts) {
+    return [{
+      // usually you will provide a callback function to be run on button click
+      // we recommend that you use a popup on click generally
+      icon: GRAY_ICON, // don't use a colored icon here
+      text: 'Open Popup',
+      callback: onBtnClick,
+      condition: 'edit'
+    }, {
+      // but of course, you could also just kick off to a url if that's your thing
+      icon: GRAY_ICON,
+      text: 'Just a URL',
+      condition: 'always',
+      url: 'https://developer.atlassian.com/cloud/trello',
+      target: 'Trello Developer Site' // optional target for above url
+    }];
+  },
+  "card-badges": function (t, opts) {
+    let cardAttachments = opts.attachments; // Trello passes you the attachments on the card
+    return t
+      .card("name")
+      .get("name")
+      .then(function (cardName) {
+        console.log("We just loaded the card name for fun: " + cardName);
+        return [
+          {
+            // Dynamic badges can have their function rerun
+            // after a set number of seconds defined by refresh.
+            // Minimum of 10 seconds.
+            dynamic: function () {
+              // we could also return a Promise that resolves to
+              // this as well if we needed to do something async first
+              return {
+                text: "Dynamic " + (Math.random() * 100).toFixed(0).toString(),
+                icon: GRAY_ICON,
+                color: "green",
+                refresh: 10, // in seconds
+              };
+            },
+          },
+          {
+            // It's best to use static badges unless you need your
+            // badges to refresh.
+            // You can mix and match between static and dynamic
+            text: "Static",
+            icon: GRAY_ICON, // for card front badges only
+            color: null,
+          },
+        ];
+      });
   }
 });
