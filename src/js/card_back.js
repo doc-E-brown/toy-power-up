5b21e38a037fa5525e0718108392ea9e1931eeda
Ben Johnston <docEbrown_github@protonmail.com>
2021-08-26 20:59:47 +1000

    feat: Test card-back-section

---
+++ src/js/card_back.js
@@ -0,0 +1,19 @@
+var GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
+
+window.TrelloPowerUp.initialize({
+  'card-back-section': function (t, options) {
+    return {
+      title: 'My Card Back Section',
+      icon: GRAY_ICON, // Must be a gray icon, colored icons not allowed.
+      content: {
+        type: 'iframe',
+        url: t.signUrl('./section.html'),
+        height: 230, // Max height is 1500.
+        action: {
+          text: 'My Action',
+          callback: (t) => t.popup({}),
+        },
+      }
+    };
+  }
+});
