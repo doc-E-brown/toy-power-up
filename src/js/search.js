ffecc64a314c055e5ec2e5f7dda9b10b29c7232d
Ben Johnston <docEbrown_github@protonmail.com>
2021-08-28 00:49:50 +1000

    feat: Add https

--- src/js/search.js
+++ src/js/search.js
@@ -3,10 +3,16 @@ var sanitize = function (value) {
   return value.replace(/ /g, '+').replace(/[^a-zA-Z0-9+]/g, '')
 }
 
+var getISDNs = function (title) {
+
+
+
+}
+
 document.querySelector("#search").addEventListener('click', function () {
 
   const title = sanitize(document.getElementById("title").value);
-  fetch('http://openlibrary.org/search.json?q=' + sanitize(title))
+  fetch('https://openlibrary.org/search.json?q=' + sanitize(title))
     .then(response => response.json())
     .then(data => console.log(data));
 
